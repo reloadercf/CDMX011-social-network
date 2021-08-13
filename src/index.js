484d3d6389ad7ff15cdbe210b66c217a69fda7b2
Isabel <isvaoc@gmail.com>
2021-08-13 14:09:13 -0500

    añadiendo pantallas, rutas y diseño

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import { onNavigate } from "./app";
+/* eslint-disable quotes */
+import { routes } from "./app.js";
 
-document.getElementById('back').addEventListener('click', onNavigate('/home'));
 
-const rootDiv = document.getElementById('root');
-rootDiv.innerHTML = routes[window.location.pathname];
+// document.getElementById("back").addEventListener("click", onNavigate("/home"));
+
+const rootDiv = document.getElementById("root");
+
+
+const element = routes[window.location.pathname];
+element(rootDiv);
+
+
+rootDiv.addEventListener("click", (event) => {
+  let target = event.target;
+});
+
+
+window.onpopstate = () => {
+  const path = routes[window.location.pathname];
+  path(rootDiv);
+};
