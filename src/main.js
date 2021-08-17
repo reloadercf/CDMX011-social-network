12456d3171dfbb9157fd85c845c77f7d02482ff2
reloadercf <elcfm1@gmail.com>
2021-08-17 18:35:13 -0500

    Add router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
-// Este es el punto de entrada de tu aplicacion
+import { Welcome } from './components/Welcome.js';
+import { Signup } from './components/Signup.js';
+import { Login } from './components/Login.js';
 
-import { myFunction } from './lib/index.js';
+const rootDiv = document.getElementById('root');
 
-myFunction();
+const routes = {
+  '/': Welcome,
+  '/signup': Signup,
+  '/login': Login,
+};
+const target = routes[window.location.pathname];
+
+function onNavigate(pathname) {
+  window.history.pushState(
+    {},
+    pathname,
+    window.location.origin + pathname,
+  );
+  while (rootDiv.firstChild) {
+    rootDiv.removeChild(rootDiv.firstChild);
+  }
+  rootDiv.appendChild(target([pathname]));
+}
+
+rootDiv.appendChild(target());
+
+window.onpopstate = () => {
+  rootDiv.appendChild(target(onNavigate));
+};
